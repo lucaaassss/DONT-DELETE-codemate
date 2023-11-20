3113430fce43caa4ac8cfdae0238887c8d74c254
shahirulprojects <shahiruliimalo@gmail.com>
2023-11-20 21:11:54 +0800

    feat:add tags at tags page

---
+++ app/(root)/tags/page.tsx
@@ -0,0 +1,70 @@
+import Filter from "@/components/shared/Filter";
+import NoResult from "@/components/shared/NoResult";
+import LocalSearchbar from "@/components/shared/search/LocalSearchbar";
+import { UserFilters } from "@/constants/filters";
+import { getAllTags } from "@/lib/actions/tag.actions";
+import Link from "next/link";
+
+const Page = async () => {
+  const result = await getAllTags({});
+
+  return (
+    <>
+      <h1 className="h1-bold text-dark100_light900">All Tags</h1>
+
+      <div className="mt-11 flex justify-between gap-5 max-sm:flex-col sm:items-center">
+        <LocalSearchbar
+          route="/tags"
+          iconPosition="left"
+          imgSrc="/assets/icons/search.svg"
+          placeholder="Search for tags"
+          otherClasses="flex-1"
+        />
+        {/* otherClasses is the classes for other pages because we will use this local searchbar component at different pages */}
+        <Filter
+          // this UserFilters is a constant retrieved from the constants > filters.ts file because we will use this filter in different pages
+          // otherClasses is the classes for other pages because we will use this local searchbar component at different pages
+          filters={UserFilters}
+          otherClasses="min-h-[56px] sm:min-w-[170px]"
+        />
+      </div>
+
+      <section className="mt-12 flex flex-wrap gap-4">
+        {result.tags.length > 0 ? (
+          result.tags.map((tag) => (
+            <Link
+              href={`tags/${tag._id}`}
+              key={tag._id}
+              className="shadow-light100_darknone"
+            >
+              <article className="background-light900_dark200 light-border flex w-full flex-col rounded-2xl border px-9 py-10 sm:w-[260px]">
+                <div className="background-light800_dark400 w-fit rounded-sm px-5 py-1.5">
+                  <p className="paragraph-semibold text-dark300_light900">
+                    {tag.name}
+                  </p>
+                </div>
+                <p className="small-medium text-dark400_light500 mt-3.5">
+                  <span className="body-semibold text-dark500_light500 mr-2.5">
+                    {tag.questions.length > 0
+                      ? `${tag.questions.length}+` // if more than 0 will have + sign
+                      : tag.questions.length}
+                  </span>
+                  Questions
+                </p>
+              </article>
+            </Link>
+          ))
+        ) : (
+          <NoResult
+            title="No Tags Found"
+            description="It looks like there are no tags found"
+            link="/ask-question"
+            linkTitle="Ask a Question"
+          />
+        )}
+      </section>
+    </>
+  );
+};
+
+export default Page;
