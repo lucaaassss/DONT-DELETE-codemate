08fe684d1a6024bb1f3ab9b5737121ad5c11556f
shahirulprojects <shahiruliimalo@gmail.com>
2023-12-05 01:58:43 +0800

    fix:deploy error

--- lib/actions/tag.actions.ts
+++ lib/actions/tag.actions.ts
@@ -60,7 +60,9 @@ export async function getQuestionsByTagId(params: GetQuestionsByTagIdParams) {
   try {
     connectToDatabase();
 
-    const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+    // const { tagId, page = 1, pageSize = 10, searchQuery } = params;
+
+    const { tagId, searchQuery } = params;
 
     const tagFilter: FilterQuery<ITag> = { _id: tagId };
 
