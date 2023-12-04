2e853af8b3c459337d907a867eba053b6d8bdd6b
shahirulprojects <shahiruliimalo@gmail.com>
2023-12-04 22:30:57 +0800

    feat:add collection page search functionality

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -164,7 +164,7 @@ export async function getSavedQuestions(params: GetSavedQuestionsParams) {
   try {
     connectToDatabase();
 
-    const { clerkId, page = 1, pageSize = 10, filter, searchQuery } = params;
+    const { clerkId, searchQuery } = params;
 
     const query: FilterQuery<typeof Question> = searchQuery // we always do it like this for searchQuery
       ? { title: { $regex: new RegExp(searchQuery, "i") } }
