5964f4d9a21fccf5d56cfe67798ccfbef133db82
shahirulprojects <shahiruliimalo@gmail.com>
2023-12-08 11:26:17 +0800

    feat:add loading state for the community page

--- lib/actions/user.action.ts
+++ lib/actions/user.action.ts
@@ -104,7 +104,7 @@ export async function getAllUsers(params: GetAllUsersParams) {
   try {
     connectToDatabase();
 
-    const { searchQuery, filter, page = 1, pageSize = 5 } = params;
+    const { searchQuery, filter, page = 1, pageSize = 6 } = params;
     const skipAmount = (page - 1) * pageSize;
 
     const query: FilterQuery<typeof User> = {};
