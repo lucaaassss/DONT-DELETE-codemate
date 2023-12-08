f163014f84ce56c6cee491d837826c7b1745e430
shahirulprojects <shahiruliimalo@gmail.com>
2023-12-09 00:28:57 +0800

    fix:surpress typescript error

--- lib/actions/question.action.ts
+++ lib/actions/question.action.ts
@@ -354,6 +354,7 @@ export async function getRecommendedQuestions(params: RecommendedParams) {
 
     // get distinct tag IDs from user's interactions
     const distinctUserTagIds = [
+      // @ts-ignore
       ...new Set(userTags.map((tag: any) => tag._id)),
     ];
 
