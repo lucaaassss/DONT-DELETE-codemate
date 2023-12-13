d80b7725858c164dfb2d51a091df22975d20a481
shahirulprojects <shahiruliimalo@gmail.com>
2023-12-13 13:43:42 +0800

    feat:add support form

--- app/(root)/support/page.tsx
+++ app/(root)/support/page.tsx
@@ -1,16 +1,135 @@
-import React from "react";
+"use client";
 
-import type { Metadata } from "next";
-
-export const metadata: Metadata = {
-  title: "Support | Codemate",
-};
+import React, { useRef } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { Button } from "@/components/ui/button";
+import emailjs from "@emailjs/browser";
+import Image from "next/image";
 
 const Support = () => {
+  const nameRef: any = useRef();
+  const emailRef: any = useRef();
+  const messageRef: any = useRef();
+
+  const sendmail = (event: any) => {
+    event.preventDefault();
+    // Check if environment variables are defined
+    const serviceId = process.env.NEXT_PUBLIC_EMAILJS_SERVICE_ID;
+    const templateId = process.env.NEXT_PUBLIC_EMAILJS_TEMPLATE_ID;
+    const publicKey = process.env.NEXT_PUBLIC_EMAILJS_PUBLIC_KEY;
+
+    console.log("Service ID:", serviceId);
+    console.log("Template ID:", templateId);
+    console.log("Public Key:", publicKey);
+
+    if (!serviceId || !templateId || !publicKey) {
+      console.error("One or more EmailJS environment variables are missing.");
+      return;
+    }
+
+    emailjs
+      .send(
+        serviceId,
+        templateId,
+        {
+          from_name: nameRef.current.value,
+          to_name: "Codemate",
+          from_email: emailRef.current.value,
+          to_email: "codematecare@gmail.com",
+          message: messageRef.current.value,
+        },
+        publicKey
+      )
+      .then(
+        () => {
+          toast.success("Message successfully sent!");
+        },
+        (error) => {
+          console.error("Error sending message:", error);
+          toast.error("Unable to send message :(");
+        }
+      );
+
+    event.target.reset();
+  };
+
   return (
-    <div>
-      <h1 className="h1-bold text-dark100_light900">Coming Soon...</h1>
-    </div>
+    <section
+      className="max-container background-light900_dark300 relative flex flex-col lg:flex-row"
+      style={{ minHeight: "100px" }}
+    >
+      <div className="flex min-w-[50%] flex-1 flex-col">
+        <h1 className="head-text">Let&apos;s hear it from you!</h1>
+        <div>
+          <Image
+            src="/assets/images/codematerobotpng.png"
+            alt="Codemate robot"
+            width={30}
+            height={30}
+            className="object-contain "
+          />
+        </div>
+        <form
+          onSubmit={sendmail}
+          className="background-light900_dark300 mt-14 flex w-full flex-col gap-7"
+        >
+          <p className="body-regular text-dark-300 dark:text-white">
+            Here at Codemate we aim to make your experience awesome. Got
+            questions or suggestions? Let us know through the form below — your
+            input matters!
+          </p>
+          <label className="font-semibold text-dark-300 dark:text-white">
+            Name
+            <input
+              type="text"
+              name="name"
+              placeholder="Enter name"
+              required
+              ref={nameRef}
+              className="input"
+            />
+          </label>
+          <label className="font-semibold text-dark-300 dark:text-white">
+            Email
+            <input
+              type="text"
+              name="email"
+              placeholder="Enter email"
+              required
+              ref={emailRef}
+              className="input"
+            />
+          </label>
+
+          <label className="font-semibold text-dark-300 dark:text-white">
+            Message
+            <textarea
+              rows={4}
+              name="message"
+              placeholder="Enter message"
+              required
+              ref={messageRef}
+              className="textarea"
+            />
+          </label>
+          <Button className="btnsupport" type="submit">
+            Send
+          </Button>
+        </form>
+        <div>
+          <p className="mt-5 text-slate-700 dark:text-slate-300">
+            Or simply email us at codematecare@gmail.com
+          </p>
+        </div>
+        <ToastContainer
+          position="top-center"
+          hideProgressBar={true}
+          theme="light"
+          autoClose={2000}
+        />
+      </div>
+    </section>
   );
 };
 
