7459439ea584e88af7210d15075eaef938d696f3
shahirulprojects <shahiruliimalo@gmail.com>
2023-12-19 00:54:33 +0800

    feat:add delete confirmation message

--- components/shared/EditDeleteAction.tsx
+++ components/shared/EditDeleteAction.tsx
@@ -4,6 +4,7 @@ import { deleteQuestion } from "@/lib/actions/question.action";
 import Image from "next/image";
 import { usePathname, useRouter } from "next/navigation";
 import { toast } from "../ui/use-toast";
+import React, { useState } from "react";
 
 interface Props {
   type: string;
@@ -13,14 +14,21 @@ interface Props {
 const EditDeleteAction = ({ type, itemId }: Props) => {
   const pathname = usePathname();
   const router = useRouter();
+  const [showDeleteModal, setShowDeleteModal] = useState(false);
 
   const handleEdit = async () => {
-    router.push(`/question/edit/${JSON.parse(itemId)}`); // will route to the edit question page
+    router.push(`/question/edit/${JSON.parse(itemId)}`);
   };
 
-  const handleDelete = async () => {
+  const handleDelete = async (event: React.MouseEvent<HTMLImageElement>) => {
+    event.preventDefault(); // to prevent the browser from refreshing and to allow smooth deletion of answer
+    setShowDeleteModal(true); // will initially display the confirmation message
+  };
+
+  const confirmDelete = async () => {
+    setShowDeleteModal(false); // regardless if the user choose yes or no,the confirmation message will be closed back
+
     if (type === "Question") {
-      // delete question
       await deleteQuestion({
         questionId: JSON.parse(itemId),
         path: pathname,
@@ -31,7 +39,6 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
         description: "Question has been successfully deleted.",
       });
     } else if (type === "Answer") {
-      // delete answer
       await deleteAnswer({
         answerId: JSON.parse(itemId),
         path: pathname,
@@ -44,6 +51,17 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
       });
     }
   };
+
+  const cancelDelete = () => {
+    setShowDeleteModal(false); // regardless if the user choose yes or no,the confirmation message will be closed back
+
+    toast({
+      title: "Delete Canceled",
+      variant: "default",
+      description: "Deletion process has been canceled.",
+    });
+  };
+
   return (
     <div className="flex items-center justify-end gap-3 max-sm:w-full">
       {type === "Question" && (
@@ -56,6 +74,7 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
           onClick={handleEdit}
         />
       )}
+
       <Image
         src="/assets/icons/bin.svg"
         alt="Delete"
@@ -64,6 +83,30 @@ const EditDeleteAction = ({ type, itemId }: Props) => {
         className="cursor-pointer object-contain"
         onClick={handleDelete}
       />
+
+      {showDeleteModal && (
+        <div className="fixed inset-0 flex items-center justify-center backdrop-blur-md">
+          <div className="rounded-[25px] bg-white p-10 dark:bg-dark-300">
+            <p className="mb-4 text-lg font-semibold text-dark-300 dark:text-white">
+              Are you sure you want to delete?
+            </p>
+            <div className="mt-6 flex items-center justify-center gap-4">
+              <button
+                className="mr-2 rounded-md bg-red-500 px-4 py-2 text-white hover:bg-red-600"
+                onClick={confirmDelete}
+              >
+                Yes
+              </button>
+              <button
+                className="rounded-md bg-light-700 px-4 py-2 text-gray-800 hover:bg-gray-300"
+                onClick={cancelDelete}
+              >
+                No
+              </button>
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
