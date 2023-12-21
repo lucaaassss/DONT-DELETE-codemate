cb1ef28e705730b33c827f2d2dc0a995e4b156ba
shahirulprojects <shahiruliimalo@gmail.com>
2023-12-22 01:44:17 +0800

    fix:flip thumbs up

--- components/shared/ChatGPTAnswer.tsx
+++ components/shared/ChatGPTAnswer.tsx
@@ -148,7 +148,7 @@ const ChatGPTAnswer = ({ question, authorId }: Props) => {
                   alt="thumbsup"
                   width={14}
                   height={14}
-                  className="invert-colors flip-vertical object-contain"
+                  className="invert-colors max-lg:flip-vertical object-contain"
                   // flip-vertical is a custom class
                 />
               </>
