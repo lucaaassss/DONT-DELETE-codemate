a6372e673f6704c809b9a4f19fb6a2a534efbd8a
shahirulprojects <shahiruliimalo@gmail.com>
2024-01-03 01:52:38 +0800

    fix:icon orientation

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -156,7 +156,7 @@ const Votes = ({
             width={18}
             height={18}
             alt="downvote"
-            className="max-sm:flip-vertical cursor-pointer"
+            className="max-md:flip-vertical cursor-pointer"
             onClick={() => handleVote("downvote")}
           />
 
