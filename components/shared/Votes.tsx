86cf29cb3db3d073ac257a7383d8fff1773766a5
shahirulprojects <shahiruliimalo@gmail.com>
2024-01-03 00:59:47 +0800

    fix:downvote icon orientation

--- components/shared/Votes.tsx
+++ components/shared/Votes.tsx
@@ -156,7 +156,7 @@ const Votes = ({
             width={18}
             height={18}
             alt="downvote"
-            className="rotate-[270] cursor-pointer max-lg:rotate-180 max-sm:rotate-[270]"
+            className="rotate-[270] cursor-pointer max-lg:rotate-180 max-sm:rotate-180"
             onClick={() => handleVote("downvote")}
           />
 
