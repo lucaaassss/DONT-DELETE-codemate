c4281f6799f82fb7a69c7d53e31aad63fce8ff78
shahirulprojects <shahiruliimalo@gmail.com>
2024-01-14 02:32:08 +0800

    styles:some styles changes

--- constants/filters.ts
+++ constants/filters.ts
@@ -27,9 +27,9 @@ export const TagFilters = [
 ];
 
 export const HomePageFilters = [
+  { name: "Popular", value: "frequent" },
   { name: "Newest", value: "newest" },
   { name: "Recommended", value: "recommended" },
-  { name: "Frequent", value: "frequent" },
   { name: "Unanswered", value: "unanswered" },
 ];
 
